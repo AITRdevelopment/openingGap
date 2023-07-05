e7a6712a6fc2b4de1ad7bb99cd671c021c5167a3
Mathieu Bartels <mathieubartels@gmail.com>
2023-07-05 14:48:48 +0200

    Add resample function

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
+
+def resample(data, sample):
+    resampled_data = (
+        data.resample(sample)
+        .agg(
+            {
+                "open": "first",
+                "high": "max",
+                "low": "min",
+                "close": "last",
+                "volume": "sum",
+            }
+        )
+        .dropna()
+    )
+    resampled_data.index.name = "datetime"
+    return resampled_data
+
+
+
 def openingsGap(data, sessionTimeDay0='1D', sessionTimeDay1='10m'):
     raise NotImplementedError()
 
